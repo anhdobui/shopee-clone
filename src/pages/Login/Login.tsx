fdc18933e8c66e2301d0ecd7ec470752d2f3e280
anhdobui <anhdobui2002@gmail.com>
2023-02-23 23:04:44 +0700

    custom class Tailwindcss

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -18,7 +18,7 @@ function Login() {
 	})
 	return (
 		<div className="bg-orange">
-			<div className="mx-auto max-w-7xl px-4">
+			<div className="container">
 				<div className="grid grid-cols-1 py-12 lg:grid-cols-5 lg:py-32 lg:pr-10">
 					<div className="lg:col-span-2 lg:col-start-4">
 						<form className="rounded bg-white p-10 shadow-sm" onSubmit={onSubmit} noValidate>
@@ -35,6 +35,7 @@ function Login() {
 							<div className="mt-3">
 								<input
 									type="password"
+									autoComplete="current-password"
 									className="w-full rounded-sm border border-gray-300 p-3 outline-none focus:border-gray-500 focus:shadow-sm"
 									placeholder="Password"
 									{...register('password', rules.password)}
