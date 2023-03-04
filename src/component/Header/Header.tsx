f2692f2d1c8f6cf759955e27b9041f751066e286
anhdobui <anhdobui2002@gmail.com>
2023-03-05 00:20:57 +0700

    use floating ui and framer motion

--- src/component/Header/Header.tsx
+++ src/component/Header/Header.tsx
@@ -1,11 +1,36 @@
+import { FloatingPortal, useFloating, arrow, shift, offset } from '@floating-ui/react'
+import { motion } from 'framer-motion'
+import { useState, useRef } from 'react'
 import { Link } from 'react-router-dom'
 
 function Header() {
+	const [isOpen, setIsOpen] = useState(false)
+	const arrowRef = useRef(null)
+	const { x, y, strategy, refs, middlewareData } = useFloating({
+		middleware: [
+			offset(6),
+			shift(),
+			arrow({
+				element: arrowRef
+			})
+		]
+	})
+	const showPopover = () => {
+		setIsOpen(true)
+	}
+	const hidePopover = () => {
+		setIsOpen(false)
+	}
 	return (
 		<div className="bg-[linear-gradient(-180deg,#f53d2d,#f63)] pb-5 pt-2 text-white">
 			<div className="container">
 				<div className="flex justify-end">
-					<div className="flex cursor-pointer items-center py-1 hover:text-gray-300">
+					<div
+						className="flex cursor-pointer items-center py-1 hover:text-gray-300"
+						ref={refs.setReference}
+						onMouseEnter={showPopover}
+						onMouseLeave={hidePopover}
+					>
 						<svg
 							xmlns="http://www.w3.org/2000/svg"
 							fill="none"
@@ -31,7 +56,41 @@ function Header() {
 						>
 							<path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
 						</svg>
+						<FloatingPortal>
+							{isOpen && (
+								<motion.div
+									ref={refs.setFloating}
+									style={{
+										position: strategy,
+										top: y ?? 0,
+										left: x ?? 0,
+										width: 'max-content',
+										transformOrigin: `${middlewareData.arrow?.x}px top`
+									}}
+									initial={{ opacity: 0, transform: 'scale(0)' }}
+									animate={{ opacity: 1, transform: 'scale(1)' }}
+									exit={{ opacity: 0, transform: 'scale(0)' }}
+									transition={{ duration: 0.2 }}
+								>
+									<span
+										ref={arrowRef}
+										className="absolute z-10 translate-y-[-95%] border-[11px] border-x-transparent border-t-transparent border-b-white"
+										style={{
+											left: middlewareData.arrow?.x,
+											top: middlewareData.arrow?.y
+										}}
+									></span>
+									<div className="relative rounded-sm border border-gray-200 bg-white shadow-md">
+										<div className="flex flex-col py-2 px-3">
+											<button className="py-2 px-3 hover:text-orange">Tiếng Việt</button>
+											<button className="mt-2 py-2 px-3 hover:text-orange">English</button>
+										</div>
+									</div>
+								</motion.div>
+							)}
+						</FloatingPortal>
 					</div>
+
 					<div className="ml-6 flex cursor-pointer items-center py-1 hover:text-gray-300">
 						<div className="mr-2 h-6 w-6 flex-shrink-0">
 							<img
